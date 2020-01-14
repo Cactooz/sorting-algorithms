7cd15bcfe99d42eae9333088ca9dadcf0b6d31e0
hugobacher <huba02001@utb.vaxjo.se>
2020-01-14 13:17:19 +0100

    Added simple swap

---
+++ sorting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            Swap(ref num1, ref num2);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
